9284feccf1509be14235a40dc7a2880d3b3674d4
matroarh <finn.85@mail.ru>
2022-09-24 15:32:25 +0300

    all

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,25 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
-def hello_view(request):
-    return HttpResponse('Hello World')
+
+class MainPageView(TemplateView):
+    template_name = "index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "news.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "login.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "doc_site.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "courses_list.html"
