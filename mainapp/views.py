15ff0dd541d5e9725a61641b62fdb9bd1d02ba2e
matroarh <finn.85@mail.ru>
2022-10-02 21:42:19 +0300

    less3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,25 +1,36 @@
 from django.views.generic import TemplateView
+from datetime import datetime
 
 
 class MainPageView(TemplateView):
-    template_name = "index.html"
+    template_name = "mainapp/index.html"
 
 
 class NewsPageView(TemplateView):
-    template_name = "news.html"
+    template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['news_title'] = 'Новость'
+        context['description'] = 'Предварительное описание новости'
+        context['news_date'] = datetime.now()
+        context['range'] = range(5)
+
+        return context
 
 
 class LoginPageView(TemplateView):
-    template_name = "login.html"
+    template_name = "mainapp/login.html"
 
 
 class ContactsPageView(TemplateView):
-    template_name = "contacts.html"
+    template_name = "mainapp/contacts.html"
 
 
 class DocSitePageView(TemplateView):
-    template_name = "doc_site.html"
+    template_name = "mainapp/doc_site.html"
 
 
 class CoursesPageView(TemplateView):
-    template_name = "courses_list.html"
+    template_name = "mainapp/courses_list.html"
